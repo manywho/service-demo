0d995ed3a590538e24904936404f347865977cca
Jonjo McKay <jonjo.mckay@manywho.com>
2016-08-19 14:09:14 +0100

    Made compatible with the v2 SDK changes

--- src/main/java/com/manywho/services/demo/Application.java
+++ src/main/java/com/manywho/services/demo/Application.java
@@ -1,11 +1,11 @@
 package com.manywho.services.demo;
 
 import com.google.inject.AbstractModule;
-import com.manywho.sdk.services.BaseApplication;
+import com.manywho.sdk.services.ServiceApplication;
 import com.manywho.services.demo.providers.Sql2oProvider;
 import org.sql2o.Sql2o;
 
-public class Application extends BaseApplication {
+public class Application extends ServiceApplication {
     public static void main(String[] args) throws Exception {
         Application application = new Application();
         application.setModule(new AbstractModule() {
