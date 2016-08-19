ab28902f00974e7a6dd1c0db1dd57d7ffe318b2b
Jonjo McKay <jonjo.mckay@manywho.com>
2016-08-19 16:37:03 +0100

    Added explicit registration of the JDBC Sqlite driver

--- src/main/java/com/manywho/services/demo/Application.java
+++ src/main/java/com/manywho/services/demo/Application.java
@@ -7,6 +7,13 @@ import javax.ws.rs.ApplicationPath;
 @ApplicationPath("/")
 public class Application extends ServiceApplication {
     public Application() {
+        try {
+            // Register the JDBC driver because Tomcat complains otherwise
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException ignored) {
+
+        }
+
         this.setModule(new ApplicationModule());
         this.initialize();
     }
