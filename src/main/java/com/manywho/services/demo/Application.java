f04adf9d628e6eb4f9b2adec05024e5aa5bdda25
Jonjo McKay <jonjo.mckay@manywho.com>
2017-02-13 14:37:50 +0000

    Updated to latest version of SDK 2.0.0-SNAPSHOT

--- src/main/java/com/manywho/services/demo/Application.java
+++ src/main/java/com/manywho/services/demo/Application.java
@@ -1,26 +1,13 @@
 package com.manywho.services.demo;
 
 import com.manywho.sdk.services.ServiceApplication;
+import com.manywho.sdk.services.servers.EmbeddedServer;
+import com.manywho.sdk.services.servers.undertow.UndertowServer;
 
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/")
 public class Application extends ServiceApplication {
-    public Application() {
-        try {
-            // Register the JDBC driver because Tomcat complains otherwise
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException ignored) {
-
-        }
-
-        this.setModule(new ApplicationModule());
-        this.initialize();
-    }
-
     public static void main(String[] args) throws Exception {
-        Application application = new Application();
-        application.setModule(new ApplicationModule());
-        application.startServer("/api/demo/1");
+        EmbeddedServer server = new UndertowServer();
+        server.addModule(new ApplicationModule());
+        server.start("/api/demo/1");
     }
 }
