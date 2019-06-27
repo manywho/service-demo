39635921f72b70455dd15d5963bc6eac93dd864a
Jacek Wojciechowski <jacek_wojciechowski@dell.com>
2019-06-27 16:13:16 +0100

    enabling the service to run on Tomcat

--- src/main/java/com/manywho/services/demo/Application.java
+++ src/main/java/com/manywho/services/demo/Application.java
@@ -1,10 +1,28 @@
 package com.manywho.services.demo;
 
-import com.manywho.sdk.services.ServiceApplication;
 import com.manywho.sdk.services.servers.EmbeddedServer;
 import com.manywho.sdk.services.servers.undertow.UndertowServer;
+import com.manywho.sdk.services.servers.Servlet3Server;
 
-public class Application extends ServiceApplication {
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/") //that's for running it on Tomcat
+public class Application extends Servlet3Server {
+
+    //that's for running it on Tomcat
+    public Application() {
+
+        // Register any Guice modules that are needed
+        this.addModule(new ApplicationModule());
+
+        // Set the Application class (the entry point to the Service, usually this class)
+        this.setApplication(Application.class);
+
+        // Initialize the application
+        this.start();
+    }
+
+    //that's for running it on Undertow
     public static void main(String[] args) throws Exception {
         EmbeddedServer server = new UndertowServer();
         server.setApplication(Application.class);
